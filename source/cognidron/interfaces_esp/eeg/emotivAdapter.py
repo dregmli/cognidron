d794e2045026f611b31c1fe243d86e8eb8f66c4a
alumno <alumno@11MacBook-Pro-de-valles-apple-11.local>
2019-06-24 12:47:55 -0500

    Limpieza de Cache

--- source/cognidron/interfaces_esp/eeg/emotivAdapter.py
+++ source/cognidron/interfaces_esp/eeg/emotivAdapter.py
@@ -37,7 +37,7 @@ class EmotivAdapter(EegInterfaz):
     # Nesesarias definiarlas previamente
     clientId = ""
     clientSecret = ""
-    profile = "gogo"
+    profile = "alguien"
     # - - - - - - - - - - - - - - - - -
 
     # Estas las define el programa por si solas
@@ -165,7 +165,7 @@ class EmotivAdapter(EegInterfaz):
                             "command": "connect",
                             "headset": "%s"
                         }
-                    }""" % self. headset
+                    }""" % self.headset
         self.enviarMensaje(mensaje)
 
 
