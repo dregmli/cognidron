8ff9045ad54c15bbc1bb3624e26ee87fcb23aad8
alumno <alumno@11MacBook-Pro-de-valles-apple-11.local>
2019-06-27 18:39:44 -0500

    pruebas nunity cubo con fuerzas

---
+++ source/simulators/Pruebas phisics/Assets/Scripts/Movimiento.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+	public float moveSpeed;
+	private Vector3 moveInput;
+	private Vector3 moveVelocity;
+	private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lh = Input.GetAxis("Horizontal");
+        float lv = Input.GetAxis("Vertical");
+
+        moveInput = new Vector3(lh, 0f, lv);
+        moveVelocity = transform.forward * moveSpeed * moveInput.sqrMagnitude;
+        Debug.Log("moveVelocity = " + transform.forward + ", " + moveSpeed + ", " + moveInput.sqrMagnitude);
+        //Debug.Log("= ", moveVelocity);
+    }
+
+    void FixedUpdate() //se ejecuta cada 20 ms
+    {
+    	rb.velocity = moveVelocity;
+    }
+}
