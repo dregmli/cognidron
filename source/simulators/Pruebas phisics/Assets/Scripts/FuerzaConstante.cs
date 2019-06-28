dae1df459c30a67f4170871850073cd7d2e403ff
alumno <alumno@11MacBook-Pro-de-valles-apple-11.local>
2019-06-28 18:59:39 -0500

    PID

---
+++ source/simulators/Pruebas phisics/Assets/Scripts/FuerzaConstante.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Para agregar lo de fuerza relativa, agregar el componente en unity.
+//mas info en : https://docs.unity3d.com/es/current/Manual/class-ConstantForce.html
+
+public class FuerzaConstante : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
